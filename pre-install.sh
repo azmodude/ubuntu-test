8245333cdca37f081148ce70addd1e2eef273375
Gordon Schulz <gordon@gordonschulz.de>
2021-09-14 19:22:00 +0200

    Move swap size calculation around

--- pre-install.sh
+++ pre-install.sh
@@ -4,6 +4,9 @@
 
 set -Eeuxo pipefail
 
+total_memory=$(lsmem | grep "^Total online memory" | \
+    sed -r 's/^Total online memory:\s+([0-9]+G)/\1/')
+
 echo -n ${LUKS_PASSWORD} | sudo cryptsetup luksFormat --type=luks2 ${DEVP}3
 echo -n ${LUKS_PASSWORD} | sudo cryptsetup luksOpen ${DEVP}3 ${DM}3_crypt
 
@@ -12,5 +15,5 @@ sudo mkfs.vfat -F 16 -n ESP ${DEVP}1
 
 sudo pvcreate /dev/mapper/${DM}3_crypt
 sudo vgcreate ubuntu-vg /dev/mapper/${DM}3_crypt
-sudo lvcreate -L ${SWAP_SIZE} -n swap_1 ubuntu-vg
+sudo lvcreate -L ${SWAP_SIZE:-${total_memory}} -n swap_1 ubuntu-vg
 sudo lvcreate -l 100%FREE -n root ubuntu-vg
